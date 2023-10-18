f524c16feced512b01fc69435cc3ebbf68c77e9e
Lady30-19 <laura.apaza1430@gmail.com>
2023-10-18 18:30:07 -0500

    v5. Diseño APP.js y AppForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import AppForm from './componentes/AppForm';
 import logo from './logo.svg';
 //import './App.css';
 import C01componente from './pagina/C01componente';
 
 function App() {
   return (
-    <div>
-      <C01componente/>
-      Hola mundo desde React...
-      <i class="large material-icons">insert_chart</i>
-      
+    <div style={{width:"350px", background:"greenyellow",padding:"10px"}}>
+     <AppForm />
+     <p>No 1 Juan Manuel  x  A</p>
+     <p>No 1 Rosa Maria  x  A</p>
+     
+     
     </div>
   );
 }
